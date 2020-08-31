7d13c551394d9f4add929945bd54b0b4b4f3d455
cfloyd252 <cfloyd252@gmail.com>
2020-08-31 12:54:12 -0700

    completed sock merchant

---
+++ sock-merchant.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString.replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+  let pairedArray = [];
+
+  for (let i=0; i < n; i++){
+    let pairIndex = ar.indexOf(ar[i], i+1);
+
+
+    if (pairIndex !== -1) {
+      ar.splice(pairIndex, 1);
+      pairedArray.push(ar[i]);
+    }
+  }
+
+  return pairedArray.length;
+
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine(), 10);
+
+  const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+  let result = sockMerchant(n, ar);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
